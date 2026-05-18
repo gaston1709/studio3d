065ab086a4ebf3977ef9197335a0f2dab3c0c10d
gaston1709 <grassogaston18@gmail.com>
2026-05-18 14:56:10 -0300

    fix: force pm2 to load .env file by adding env_file property

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -4,6 +4,7 @@ module.exports = {
       name: "s3d",
       script: "npm",
       args: "start",
+      env_file: ".env",
       env_production: {
         NODE_ENV: "production",
         PORT: 3010
