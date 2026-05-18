a5404f044bfa08743047d56aebd9af0a28f15507
gaston1709 <grassogaston18@gmail.com>
2026-05-18 15:21:22 -0300

    fix: explicitly pass NEXTAUTH_URL and NEXTAUTH_SECRET to pm2 env_production

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -7,7 +7,9 @@ module.exports = {
       env_file: ".env",
       env_production: {
         NODE_ENV: "production",
-        PORT: 3010
+        PORT: 3010,
+        NEXTAUTH_URL: "https://studio3d.sie.com.ar",
+        NEXTAUTH_SECRET: "jddRfoAdsQJYNvnGQ/KlXtoINY0Yc/Ab0MYgdg5J2IQ="
       }
     }
   ]
